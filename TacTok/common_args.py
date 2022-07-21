6ad5ab248d87181ce1c0fbf1a8c815932f4a47d0
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2022-07-21 15:47:52 -0400

    Add option to force subwords to use globals vocab
    Even when vocabs are merged.

--- TacTok/common_args.py
+++ TacTok/common_args.py
@@ -40,6 +40,7 @@ def add_common_args(parser: argparse.ArgumentParser):
     parser.add_argument('--ident-vec-size', type=int, default=32)
     parser.add_argument('--max-ident-chunks', type=int, default=8)
     parser.add_argument('--include-unks', action='store_true')
+    parser.add_argument('--subwords-from-globals', action='store_true')
     parser.add_argument('--save-subwords', type=str, default="names/subwords.json")
     parser.add_argument('--load-subwords', type=str, default="names/subwords.json")
     parser.add_argument('--case-insensitive-idents', action='store_true')
