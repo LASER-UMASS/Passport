08edb305498e208d3e2b0cca1a192d6d15c15ca4
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2022-07-25 14:17:46 +0000

    Don't look for missing syntax attribute

--- gallina.py
+++ gallina.py
@@ -77,8 +77,7 @@ class GallinaTermParser:
                     children.append(c)
                 # Don't erase fully-qualified definition & theorem names
                 elif (((syn_conf.include_defs or keep_constructor_desc) and SyntaxConfig.is_label(node)) or
-                ((syn_conf.include_paths or keep_constructor_desc) and SyntaxConfig.is_path(node)) or
-                (syn_conf.merge_vocab and syn_conf.include_locals and SyntaxConfig.is_local(node))):
+                ((syn_conf.include_paths or keep_constructor_desc) and SyntaxConfig.is_path(node))):
                     ident_wrapper = SyntaxConfig.singleton_ident(c.value)
                     node.height = 2
                     children.append(ident_wrapper)
